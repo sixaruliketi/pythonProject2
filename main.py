1d3554711b302e51e766422c812a6dcb9e90d0a2
DESKTOP-LNFCMVA\HP <sixaruliketi@gmail.com>
2023-01-19 20:45:56 +0400

    scrapping from wiki: human-computer interaction

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+URL = "https://en.wikipedia.org/wiki/Human%E2%80%93computer_interaction"
 
+r = requests.get(URL)
 
+soup = BeautifulSoup(r.content, 'html5lib')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print('PyCharm')
+#print(soup.prettify())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+urls = []
+if r.status_code == 200:
+    href = soup.find('div', id="content").find_all('a')
+    for obj in href:
+        st = str(obj).split()
+        for o in st:
+            if o.startswith('href') :
+                urls.append("https://en.wikipedia.org" + o[6:-1])
+myUrls = list(set(urls))
+#print(myUrls)
+
+urls = ""
+filename = open("urls_from_wiki.txt", "a")
+for url in myUrls:
+    urls = str(url) + "\n"
+    filename.write(urls)
+filename.close()
